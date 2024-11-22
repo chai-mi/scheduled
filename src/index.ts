3c504e35ce13f8f0d8bdbdd771f8fec8f27ecbb2
chai-mi <107172917+chai-mi@users.noreply.github.com>
2024-11-22 16:43:37 +0800

    更新：按不同 cron 触发不同任务

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,15 @@ import { Hoyolab } from "./hoyolab"
 
 export default {
 	async scheduled(event: ScheduledController, env: Env, ctx: ExecutionContext) {
-		Hoyolab(env, ctx)
+		switch (event.cron) {
+			case '0 0 * * *':
+				return await Promise.all([
+					Hoyolab(env, ctx),
+				])
+			default:
+				// 测试用，例：
+				// await Hoyolab(env, ctx)
+				return
+		}
 	},
 }
