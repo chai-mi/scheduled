8c0c0cce324c30587b001721a186b7973bfc3c91
chai-mi <107172917+chai-mi@users.noreply.github.com>
2025-06-30 21:41:21 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,10 @@
 import { Hoyolab } from "./mihoyo"
-import { CronCheckElec, meido } from "./meido"
-import { DeleteOldDeployments } from "./pages"
 
 export default {
-    async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-        const { pathname } = new URL(request.url)
-        if (pathname === env.meidopath && request.method === "POST") {
-            ctx.waitUntil(meido.exec(env, await request.json()))
-            return new Response(null, { status: 200 })
-        }
-        return new Response(null, { status: 404 })
-    },
     async scheduled(event: ScheduledController, env: Env, ctx: ExecutionContext) {
         switch (event.cron) {
             case "0 0 * * *":
                 ctx.waitUntil(Hoyolab(env, ctx))
-                // ctx.waitUntil(DeleteOldDeployments(env, ctx))
-                return
-            case "0 7/8 * * *":
-                await CronCheckElec(env)
                 return
         }
     },
